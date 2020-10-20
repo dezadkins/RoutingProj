463de66f23a51e9bae5db4ec673d389bfb557298
dezadkins <desmondadkins@gmail.com>
2020-10-20 14:36:38 -0700

    15 specs passed

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = require("./routes");
+
+const app = express();
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+    res.send("Hello from Express!");
+});
+
+app.get("/*xyz", (req, res) => {
+    res.send("That's all I wrote.");
+});
+
+app.get("/capital-letters/:path", (req, res) => {
+    let path = req.params.path;
+    res.send(path.toUpperCase());
+});
+
+app.use(["/margot", "/margeaux"], router);
+
+/*  app.get(/^\/(capital-letters)\/(.*)/, (req, res) => {
+    let path = req.params[1];
+    res.send(path.toUpperCase());
+})  */
+
+app.all("*", (req, res) => {
+    const pageData = {
+        path: req.path,
+        method: req.method,
+        randomNumber: Math.floor(Math.random() * 100),
+    };
+    res.render("layout", pageData);
+});
+
+const port = 8081;
+app.listen(port, () => console.log("Listening on port " + port));
